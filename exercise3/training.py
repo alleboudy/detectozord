f555a453e86ec89db80427f40613a02fe4109b88
ga84ban <walter.zimmer@tum.de>
2017-05-20 13:29:07 +0200

    initial commit exercise4

--- exercise3/training.py
+++ exercise3/training.py
@@ -1,10 +1,10 @@
 import numpy as np
 import tensorflow as tf
-#%matplotlib inline
-#import matplotlib.pyplot as plt
+# %matplotlib inline
+# import matplotlib.pyplot as plt
 import random
 import os
-#import urllib.request
+# import urllib.request
 import tarfile
 import pickle
 import utils
@@ -15,40 +15,37 @@ from scipy.ndimage.filters import gaussian_filter
 data_path = "data/CIFAR-10/"
 data_url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
 
-
 # Let's start a Session
 sess = tf.Session()
 
-#load_cifar('data/CIFAR-10/'')
-DATA_AUGMENTATION=False
+# load_cifar('data/CIFAR-10/'')
+DATA_AUGMENTATION = False
 
+# train_samples, train_labels, val_samples, val_labels = load_cifar(data_path)
 train_samples, train_labels, val_samples, val_labels = load_cifar(data_path)
 
 if DATA_AUGMENTATION:
-	augmentedSamples=[]
-	augmentedLabels=[]
-	for i in range(train_samples.shape[0]):
-	    augmentedSamples.append(train_samples[i,:,:,:])
-	    augmentedSamples.append(np.flipud(train_samples[i,:,:,:]))
-	    augmentedSamples.append(np.fliplr(train_samples[i,:,:,:]))
-	    augmentedSamples.append(np.fliplr(np.flipud(train_samples[i,:,:,:])))
-	    augmentedSamples.append(gaussian_filter(train_samples[i,:,:,:], sigma=0.5))
-	    augmentedLabels.append(train_labels[i])
-	    augmentedLabels.append(train_labels[i])
-	    augmentedLabels.append(train_labels[i])
-	    augmentedLabels.append(train_labels[i])
-	    augmentedLabels.append(train_labels[i])
-    
-	train_samples = np.asarray(augmentedSamples)
-	train_labels = np.asarray(augmentedLabels)
+    augmentedSamples = []
+    augmentedLabels = []
+    for i in range(train_samples.shape[0]):
+        augmentedSamples.append(train_samples[i, :, :, :])
+        augmentedSamples.append(np.flipud(train_samples[i, :, :, :]))
+        augmentedSamples.append(np.fliplr(train_samples[i, :, :, :]))
+        augmentedSamples.append(np.fliplr(np.flipud(train_samples[i, :, :, :])))
+        augmentedSamples.append(gaussian_filter(train_samples[i, :, :, :], sigma=0.5))
+        augmentedLabels.append(train_labels[i])
+        augmentedLabels.append(train_labels[i])
+        augmentedLabels.append(train_labels[i])
+        augmentedLabels.append(train_labels[i])
+        augmentedLabels.append(train_labels[i])
 
+    train_samples = np.asarray(augmentedSamples)
+    train_labels = np.asarray(augmentedLabels)
 
-
-
-label_to_name = ['airplane', 'automobile', 'bird', 'cat', 'deer' , 'dog', 'frog', 'horse', 'ship', 'truck']
+label_to_name = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 indeces = random.sample(range(0, train_samples.shape[0]), 5)
 # TODO plot 5 training samples
-#for index in indeces:
+# for index in indeces:
 #    plt.figure(figsize=(1.5,1.5))
 #    plt.title(label_to_name[train_labels[index]])
 #    plt.imshow(train_samples[index])
@@ -63,30 +60,28 @@ NUM_CLASSES = 10
 
 # You can also modify these hyper-parameters (batch_size, epochs)
 # e.g. Add more epochs, if not converged. Reduce batch_size if too big for your GPU memory
-batch_size = 100  
+batch_size = 100
 num_train_epochs = 30
-steps_per_epoch = int( train_samples.shape[0] / batch_size)
-
+steps_per_epoch = int(train_samples.shape[0] / batch_size)
 
 # TODO define placeholders for the inputs and their labels (hint: what is their type, shape?)
 
-inputs = tf.placeholder(tf.float32,name='input',shape=[batch_size,WIDTH,HEIGHT,CHANNELS])
-labels = tf.placeholder(tf.int32,name='output',shape=[batch_size])
-keep_prob = tf.placeholder(tf.float32) #for dropout
-logits = buildNetwork(inputs, batch_size,keep_prob=keep_prob)
-
+inputs = tf.placeholder(tf.float32, name='input', shape=[batch_size, WIDTH, HEIGHT, CHANNELS])
+labels = tf.placeholder(tf.int32, name='output', shape=[batch_size])
+keep_prob = tf.placeholder(tf.float32)  # for dropout
+logits = buildNetwork(inputs, batch_size, keep_prob=keep_prob)
 
 # TODO add an appropriate cross entropy loss function
-loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels,logits=logits,name='crossEntropy')
+loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits, name='crossEntropy')
 
 # TODO use reduce_mean to combine the single loss values from all the different input samples
-meanLoss= tf.reduce_mean(loss)
+meanLoss = tf.reduce_mean(loss)
 
 # TODO add L2 regularization
-alpha=0.01
-tvars =tf.trainable_variables()
+alpha = 0.01
+tvars = tf.trainable_variables()
 l2_reg = tf.reduce_sum([tf.nn.l2_loss(var) for var in tvars])
-loss = meanLoss+alpha*l2_reg
+loss = meanLoss + alpha * l2_reg
 # hint: use tf.trainable_variables() and tf.nn.l2_loss(var)
 
 
@@ -97,22 +92,22 @@ correct_mask = tf.to_float(tf.equal(predictions, labels))
 accuracy = tf.reduce_mean(correct_mask)
 
 # TODO define the learning rate
-learningRate=.001
+learningRate = .001
 # TODO define the optimizer (experiment with different options)
-opt = tf.train.AdamOptimizer( learning_rate=learningRate,
-    beta1=0.9,
-    beta2=0.999,
-    epsilon=1e-08,
-    use_locking=False,
-    name='Adam')
+opt = tf.train.AdamOptimizer(learning_rate=learningRate,
+                             beta1=0.9,
+                             beta2=0.999,
+                             epsilon=1e-08,
+                             use_locking=False,
+                             name='Adam')
 # TODO build the corresponding training operation
 opt_op = opt.minimize(loss)
 
 # Define summary operations. Note, that the names may vary depending on how you defined variables!
 
 train_summaries = [
-  tf.summary.scalar('train_loss', loss),
-  tf.summary.scalar('train_accuracy', accuracy)
+    tf.summary.scalar('train_loss', loss),
+    tf.summary.scalar('train_accuracy', accuracy)
 ]
 train_summary_op = tf.summary.merge(train_summaries)
 
@@ -129,85 +124,79 @@ sess.run(model)
 
 
 def gen_data(source):
-    indices = range(len(source[0]))
+    indices = list(range(len(source[0])))
     random.shuffle(indices)
 
     while True:
-	#indices = range(len(source[0]))
-	#random.shuffle(indices)
+        # indices = range(len(source[0]))
+        # random.shuffle(indices)
         for i in indices:
-	    
             image = source[0][i]
-            #label=np.zeros(NUM_CLASSES)
-	    label=source[1][i]
+            # label=np.zeros(NUM_CLASSES)
+            label = source[1][i]
             yield image, label
 
 
-def gen_data_batch(source,batchSize):
+def gen_data_batch(source, batchSize):
     data_gen = gen_data(source)
-    #for i in range(numberOfBatches):
-    while(True):
+    # for i in range(numberOfBatches):
+    while (True):
         images_batch = []
         labels_batch = []
         for _ in range(batchSize):
             img, label = next(data_gen)
             images_batch.append(img)
-            #plt.figure(figsize=(1.5,1.5))
-            #plt.title(label_to_name[label])
-            #plt.imshow(img)
+            # plt.figure(figsize=(1.5,1.5))
+            # plt.title(label_to_name[label])
+            # plt.imshow(img)
             labels_batch.append(label)
-        #print("batch served!")
-        yield np.array(images_batch),np.asarray(labels_batch)
+        # print("batch served!")
+        yield np.array(images_batch), np.asarray(labels_batch)
 
 
 # indicators on how often to create a summary
-num_steps_per_train_summary = 5  
+num_steps_per_train_summary = 5
 
 # TODO run num_train_steps iterations on the training samples each of batchsize 50
-#samples_indices = list(range(train_samples.shape[0]))
+# samples_indices = list(range(train_samples.shape[0]))
 for epoch in range(num_train_epochs):
-    #print("epoch")
-    print('epoch #'+str(epoch))
-    
-    
+    # print("epoch")
+    print('epoch #' + str(epoch))
+
     # TODO randomly shuffle training samples
-    #random.shuffle(samples_indices)
+    # random.shuffle(samples_indices)
     for iteration in range(steps_per_epoch):
-        #print('iter')
-        #print(iter)
+        # print('iter')
+        # print(iter)
         # TODO read the batch and execute one iteration of the graph 
-        Xs,Ys = next(gen_data_batch([train_samples,train_labels],batch_size))
-        #print('\r')
-	#print(Ys)    
-        l,summary, _  =  sess.run([loss, train_summary_op, opt_op], feed_dict={inputs: Xs, labels: Ys,keep_prob:1.})
-#	print(epoch)
-	#print(summary)
-	print('\rEpoch: '+str(epoch)+'_Iteration: '+str(iteration)+' Loss: '+str(l))
-    
-    
-        # TODO every num_steps_per_train_summary iterations: 
+        Xs, Ys = next(gen_data_batch([train_samples, train_labels], batch_size))
+        # print('\r')
+        # print(Ys)
+        l, summary, _ = sess.run([loss, train_summary_op, opt_op], feed_dict={inputs: Xs, labels: Ys, keep_prob: 1.})
+        #	print(epoch)
+        # print(summary)
+        print('\rEpoch: ' + str(epoch) + '_Iteration: ' + str(iteration) + ' Loss: ' + str(l))
+
+        # TODO every num_steps_per_train_summary iterations:
         #     save the current training status (loss and accuracy) to tensorboard
-        if epoch % num_steps_per_train_summary==0:
+        if epoch % num_steps_per_train_summary == 0:
             writer.add_summary(summary, global_step=epoch)
-            
-    
+
     writer.add_summary(summary, global_step=epoch)
     # TODO every epoch: 
-    validation_steps = int(val_samples.shape[0]/batch_size)
-    acc=0
+    validation_steps = int(val_samples.shape[0] / batch_size)
+    acc = 0
     for valStep in range(validation_steps):
-	Xs,Ys=next(gen_data_batch([val_samples,val_labels],batch_size))
-	res=  sess.run([ accuracy], feed_dict={inputs: Xs, labels: Ys,keep_prob:1.})
-	print(res)
-	acc+=res[0]
-	print('val accuracy so far: '+str(acc))
-    summary=sess.run([ val_summary_op], feed_dict={val_accuracy: acc})
-    writer.add_summary(summary[0], global_step=epoch)
-    #     save the current validation accuracy to tensorboard
-    # Note: we are interested in the accuracy over the *entire* validation set, not just the current batch
+        Xs, Ys = next(gen_data_batch([val_samples, val_labels], batch_size))
+        res = sess.run([accuracy], feed_dict={inputs: Xs, labels: Ys, keep_prob: 1.})
+        print(res)
+        acc += res[0]
+        print('val accuracy so far: ' + str(acc))
+        summary = sess.run([val_summary_op], feed_dict={val_accuracy: acc})
+        writer.add_summary(summary[0], global_step=epoch)
+        #     save the current validation accuracy to tensorboard
+        # Note: we are interested in the accuracy over the *entire* validation set, not just the current batch
 # TODO use tf.train.Saver to save the trained model as checkpoints/model.ckpt
-#{'tvars': tvars}
+# {'tvars': tvars}
 saver = tf.train.Saver()
 saver.save(sess, 'checkpoints/model.ckpt')
-
-
