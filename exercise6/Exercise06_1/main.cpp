403e572a4a06284c0b6fff13d66ea40d158d2ecc
ga84ban <walter.zimmer@tum.de>
2017-06-10 14:34:24 +0200

    loading point cloud
    Former-commit-id: 3e3a50f37563bba4080522f03276683d4d9c0fa2

--- exercise6/Exercise06_1/main.cpp
+++ exercise6/Exercise06_1/main.cpp
@@ -4,26 +4,30 @@
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/features/normal_3d.h>
 
-int
-main (int argc, char** argv)
+using namespace plc;
+using namespace io;
+using namespace std;
+
+int main(int argc, char **argv)
 {
     std::string projectSrcDir = PROJECT_SOURCE_DIR;
-    
+
     // Load [.pcd::] file to pcl::PointCloud
     std::string modelFilename = projectSrcDir + "/data/bun0.pcd";
-    
-    
+
     // Visualize the point clouds
-    
-    
+    // create a cloud object to store the point cloud
+    PointCloud<PointXYZ>::Ptr cloud(new PointCloud<PointXYZ>);
+    if (loadPCDFile<PointXYZ>(modelFilename, *cloud) == -1)
+    {
+        PLC_ERROR("Couldn't read file: " + modelFilename);
+        return (-1);
+    }
+    cout << "Loaded" << cloud->width * cloud->height << "points" <<endl;
+
     // Compute normals of the point clouds
-    
-    
+
     // Visualize the point clouds with computed normals
-   
-    
+
     return 0;
 }
-
-
-
