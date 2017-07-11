3b1064ee7342b0958dac8219b5944480f431895b
alleboudy <ga48wed@mytum.de>
2017-07-11 12:32:49 +0200

    added pointnet, under pointnet/utils/challenge_prep.py is a script to prepare our own data generated from the main directory utils project  for the training
    Former-commit-id: 03ca48119c5ea3814c758c936426cefdd0d28b5f

---
+++ pointnet/utils/challenge_prep.py
@@ -0,0 +1,63 @@
+from plyfile import (PlyData, PlyElement, make2d, PlyParseError, PlyProperty)
+from random import shuffle
+import numpy as np
+import h5py
+# Write numpy array data and label to h5_filename
+def save_h5_data_label_normal(h5_filename, data, label, normal, 
+        data_dtype='float32', label_dtype='uint8', normal_dtype='float32'):
+    h5_fout = h5py.File(h5_filename)
+    h5_fout.create_dataset(
+            'data', data=data,
+            compression='gzip', compression_opts=4,
+            dtype=data_dtype)
+    h5_fout.create_dataset(
+            'normal', data=normal,
+            compression='gzip', compression_opts=4,
+            dtype=normal_dtype)
+    h5_fout.create_dataset(
+            'label', data=label,
+            compression='gzip', compression_opts=1,
+            dtype=label_dtype)
+    h5_fout.close()
+# Load PLY file
+def load_ply_data(filename):
+    plydata = PlyData.read(filename)
+    pc = plydata['vertex'].data
+    pcxyz_array=[]
+    pcnxyz_array=[]
+    sampled_pcxyz_array=[]
+    sampled_pcnxyz_array=[]
+    for x,y,z,_nx,_ny,_nz,_r,_g,_b,_a in pc:
+        pcxyz_array.append([x, y, z])
+        pcnxyz_array.append([_nx,_ny,_nz])
+    indices = list(range(len(pcxyz_array)))
+    indicessampled= np.random.choice(indices, size=2048)
+    for i in indicessampled:
+        sampled_pcxyz_array.append(pcxyz_array[i])
+        sampled_pcnxyz_array.append(pcnxyz_array[i])
+
+    return np.asarray(sampled_pcxyz_array),np.asarray(sampled_pcnxyz_array)
+mainplyDir='E:\\challenges\\challenge_train\\models\\'
+plyfiles2load=['bird-.ply','bond-.ply','can-.ply','cracker-.ply','shoe-.ply','teapot-.ply']
+outputh5FilePath='lol.h5'
+labelsMap = dict({"bird":0,"bond":1,"can":2,"cracker":3,"house":4,"shoe":5,"teapot":6})
+
+allpoints=[]
+allnormals=[]
+alllabels=[]
+for plyFile in plyfiles2load:
+    print(plyFile)
+    plyxyz,plynxyz = load_ply_data(mainplyDir+plyFile)
+    allpoints.append(plyxyz)
+    allnormals.append(plynxyz)
+    alllabels.append(np.asarray([labelsMap[plyFile.split('-')[0]]]))
+indices=list(range(len(allpoints)))
+shuffle(indices)
+
+allpoints_shuffle = [allpoints[i] for i in indices] 
+allnormals_shuffle = [allnormals[i] for i in indices] 
+alllabels_shuffle = [alllabels[i] for i in indices] 
+
+
+
+save_h5_data_label_normal(outputh5FilePath,np.asarray(allpoints_shuffle),np.asarray(alllabels_shuffle),np.asarray(allnormals_shuffle))
